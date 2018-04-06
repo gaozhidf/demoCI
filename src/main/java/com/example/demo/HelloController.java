c65b914e1f42823d898a73a38291059ddaccbd30
gaozhidf <gaozhidf@gmail.com>
2018-04-06 14:42:40 +0800

    project init

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model,
+                        @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}

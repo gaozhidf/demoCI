33094935d2eeb1940b1bc5630471d5a68400bff7
gaozhidf <gaozhidf@gmail.com>
2018-04-06 15:08:22 +0800

    package sources into war and add CI job

---
+++ README.md
@@ -0,0 +1,14 @@
+# demoCI
+[![Build Status](https://travis-ci.org/gaozhidf/demoCI.svg?branch=master)](https://travis-ci.org/gaozhidf/demoCI)
+
+## Build
+```shell
+# linux
+mvnw package
+
+# windows
+mvnw.cmd package
+```
+
+## Deploy
+[https://travis-ci.org/gaozhidf/demoCI](https://travis-ci.org/gaozhidf/demoCI)
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}

619a3b3c40422ae5f73ed06dde023acd62bf567e
gaozhidf <gaozhidf@gmail.com>
2018-04-08 02:49:46 +0800

    update deploy.sh

--- dockerfiles/deploy.sh
+++ dockerfiles/deploy.sh
@@ -5,6 +5,6 @@ ssh -oPort=$TARGET_SERVER_PORT  \
     -o StrictHostKeyChecking=no \
     travis@$TARGET_SERVER_HOST \
     'cd ~ && \
-     wget https://github.com/gaozhidf/demoCI/blob/master/docker-compose.yml -O docker-comose.yml&& \
+     wget https://raw.githubusercontent.com/gaozhidf/demoCI/master/docker-compose.yml -O docker-comose.yml&& \
      docker-compose down --rmi all && \
      docker-compose up -d'

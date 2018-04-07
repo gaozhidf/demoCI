8c1a978ba64121db4f4e289ae09d0f173c12f78f
gaozhidf <gaozhidf@gmail.com>
2018-04-08 01:55:29 +0800

    update deploy.sh

---
+++ dockerfiles/deploy.sh
@@ -0,0 +1,10 @@
+# remove build path from docker-compose.yml
+echo "$(grep -v "build:" docker-compose.yml)" > docker-compose.yml
+
+ssh -oPort=$TARGET_SERVER_PORT  \
+    -o StrictHostKeyChecking=no \
+    travis@$TARGET_SERVER_HOST \
+    'cd ~ && \
+     wget https://github.com/$TRAVIS_REPO_SLUG/blob/master/docker-compose.yml && \
+     docker-compose down --rmi all && \
+     docker-compose up -d'

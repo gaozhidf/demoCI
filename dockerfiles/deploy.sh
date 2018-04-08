e1f4df62c4f510fa023cc657aa8aefffd123e9e4
ubuntu <ubuntu@localhost.localdomain>
2018-04-08 13:01:23 +0800

    update deploy.sh

--- dockerfiles/deploy.sh
+++ dockerfiles/deploy.sh
@@ -1,11 +1,10 @@
-# remove build path from docker-compose.yml
-echo "$(grep -v "build:" docker-compose.yml)" > docker-compose.yml
 
 ssh -oPort=$TARGET_SERVER_PORT  \
     -o StrictHostKeyChecking=no \
     travis@$TARGET_SERVER_HOST \
-    'cd ~ && \
-     rm  -f docker-comose.yml && \
-     wget https://raw.githubusercontent.com/gaozhidf/demoCI/master/docker-compose.yml&& \
+    'cd ~ && source .profile && \
+     rm  -f docker-compose.yml && \
+     wget https://raw.githubusercontent.com/gaozhidf/demoCI/master/docker-compose.yml && \
+     echo "$(grep -v "build:" docker-compose.yml)" > docker-compose.yml && \
      docker-compose down --rmi all && \
      docker-compose up -d'
